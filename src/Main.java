41245a5b06a1ad986c1472f15747a8dcee180ab0
Tom <tommalexandre@gmail.com>
2025-04-09 14:13:04 +0200

    Changement DB

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bonjour à tous les amis!, je suis accompagné de Miss Jirachy.");
+        System.out.println("Bonjour à tous les amis!, je suis accompagné de Miss Jirachi.");
     }
 }
