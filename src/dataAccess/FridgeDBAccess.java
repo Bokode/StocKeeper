ae24e68ee3accfb045d813c11b80b4560d8a09f1
AuguC <zerix2909@gmail.com>
2025-04-24 15:50:58 +0200

    mot de passe en clair supprimer

--- src/dataAccess/FridgeDBAccess.java
+++ src/dataAccess/FridgeDBAccess.java
@@ -8,7 +8,7 @@ public class FridgeDBAccess {
 
     private static final String URL = "jdbc:mysql://localhost:3306/stocKeeper?serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASSWORD = "20JGRL9q9"; // Remplace par ton vrai mot de passe
+    private static final String PASSWORD = "password"; // Remplace par ton vrai mot de passe
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
