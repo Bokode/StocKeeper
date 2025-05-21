62bd425d2dfece3469d8302c72d7cac5689f6922
Alepok5 <alexandre.laccroix@gmail.com>
2025-05-21 20:45:52 +0200

    deleteRecipePanel should be done

--- src/ViewPackage/Main.java
+++ src/ViewPackage/Main.java
@@ -11,7 +11,7 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) throws SQLException {
         //App
-        //MainWindow mainWindow = new MainWindow();
+        MainWindow mainWindow = new MainWindow();
 
         // Variable
         LocalDate localDate = LocalDate.now();
