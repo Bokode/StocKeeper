af4be9d1c1f367ab047c905c85b0ff3ecf15f1eb
Tom <tommalexandre@gmail.com>
2025-05-22 20:15:34 +0200

    Oublié un

--- src/controllerPackage/TypeMaterialController.java
+++ src/controllerPackage/TypeMaterialController.java
@@ -1,6 +1,7 @@
 package controllerPackage;
 
 import businessPackage.TypeMaterialManager;
+import exceptionPackage.AppException;
 
 public class TypeMaterialController {
     private TypeMaterialManager typeMaterialManager;
@@ -11,5 +12,21 @@ public class TypeMaterialController {
 
     public void setTypeMaterialManager(TypeMaterialManager typeMaterialManager) {
         this.typeMaterialManager = typeMaterialManager;
+    }
+
+    public void addTypeMaterial(String label) throws AppException {
+        typeMaterialManager.addTypeMaterial(label);
+    }
+
+    public void deleteTypeMaterial(String label) throws AppException {
+        typeMaterialManager.deleteTypeMaterial(label);
+    }
+
+    public int getIdByLabel(String label) throws AppException {
+        return typeMaterialManager.getIdByLabel(label);
+    }
+
+    public String getLabelById(int id) throws AppException {
+        return typeMaterialManager.getLabelById(id);
     }
 }
